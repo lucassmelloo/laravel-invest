24fab251810d1d70a5d8c302a451181df19cea9f
Mello <lucasmellovieira99@gmail.com>
2023-05-04 22:49:41 -0300

    Finalizando filtro dos investimentos de renda fixas

--- resources/views/fixed_incomes/partials/components/filter_fixed_incomes.blade.php
+++ resources/views/fixed_incomes/partials/components/filter_fixed_incomes.blade.php
@@ -10,37 +10,41 @@
                     {{ __('You can register your fidex incomes and filter then.') }}
                 </p>
             </header>
-            <form method="GET" action="{{ route('product_type.create') }}" class="flex ">
-                @csrf
-                @method('GET')
-                <div class="w-1/2 px-3">
-                    <x-input-label for="indexer" :value="__('Indexers')" />
-                    <select id="indexer" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                        <option selected>Choose indexer</option>
-                        @foreach ( $indexers as $indexer)  
-                            <option value="{{$indexer->id}}">{{$indexer->abreviation}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="w-1/2 px-3">
-                    <x-input-label for="productType" :value="__('Product Type')" />
-                    <select id="productType" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                        <option selected>Choose product type</option>
-                        @foreach ( $product_types as $product_type)  
-                            <option value="{{$product_type->id}}">{{$product_type->abreviation}}</option>
-                        @endforeach
-                    </select>
-                </div>
+            <div class="flex">
+                <form method="GET" action="{{ route('product_type.create') }}" class="flex w-full">
+                    @csrf
+                    @method('GET')
+                    <div class="w-3/5 pr-3">
+                        <x-input-label for="description" :value="__('Description')" />
+                        <x-text-input id="description" name="description" type="text" class="block w-full" required autofocus autocomplete="description" />
+                    </div>
+                    <div class="w-1/5 pr-3">
+                        <x-input-label for="indexer" :value="__('Indexers')" />
+                        <select id="indexer" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                            <option selected>Choose indexer</option>
+                            @foreach ( $indexers as $indexer)  
+                                <option value="{{$indexer->id}}">{{$indexer->abreviation}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="w-1/5 pr-3">
+                        <x-input-label for="productType" :value="__('Product Type')" />
+                        <select id="productType" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                            <option selected>Choose product type</option>
+                            @foreach ( $product_types as $product_type)  
+                                <option value="{{$product_type->id}}">{{$product_type->abreviation}}</option>
+                            @endforeach
+                        </select>
+                    </div>
 
-                <div>
-                    <x-input-label for="description" :value="__('Description')" />
-                    <x-text-input id="description" name="description" type="text" class="mt-1 block w-full" required autofocus autocomplete="description" />
+                    <div class="flex items-center gap-4 mt-5 pr-3">
+                        <x-secondary-button>{{ __('Filter') }}</x-primary-button>
+                    </div>
+                </form>
+                <div class="flex items-center gap-4 mt-5 pr-3">
+                    <x-primary-button>{{ __('New') }}</x-primary-button>
                 </div>
-
-                <div class="flex items-center gap-4">
-                    <x-primary-button>{{ __('Filter') }}</x-primary-button>
-                </div>
-            </form>
+            </div>
         </section>
     </div>
 </div>
