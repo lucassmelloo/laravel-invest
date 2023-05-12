1b613b55dc16c838f31f761d20de0886e719e6df
Mello <lucasmellovieira99@gmail.com>
2023-05-11 23:45:22 -0300

    Criando tabela value_records e coluna quotas

---
+++ database/migrations/2023_05_12_020812_create_value_records_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\FixedIncomes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('value_records', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('record_date');
+            $table->float('record_liquid_value');
+            $table->float('record_gross_value')->nullable();
+            $table->foreignIdFor(FixedIncomes::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('value_records');
+    }
+};
